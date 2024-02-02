be7e2a8e472bd02e4a4b9426ff08f657807027f4
Tyler <penguincavalry@gmail.com>
2024-02-02 11:21:36 -0700

    It's real close, the resets are the issue.

--- card.js
+++ card.js
@@ -1,14 +1,4 @@
-import { randomiseEntree,randomiseSide,randomiseVegetable } from "./app.js"
-
-
-
-// window.onload = function() {
-//   const randomButton = document.getElementById("randomiseButton");
-//   console.log(randomButton)
-//   randomButton.addEventListener('click', detectButton(mainDish, i))
-// }
-
-// fix the icon https://icons.getbootstrap.com/icons/dice-6-fill/
+import { randomiseEntree,randomiseSide,randomiseVegetable} from "./app.js"
 
 export function cardHtml(mainDish,i){
 return `<div>
@@ -16,30 +6,23 @@ return `<div>
         <h2>${mainDish[i].category}</h2>
         <div class="d-flex gap-2 mb-3">
           <button id=btnRec-${i} type="button" class="btn btn-primary randomiseButton">
-          <i class="bi bi-dice-6-fill">dice</i>
-          Roll'Em!
+            <i class="bi bi-dice-6-fill">Roll'Em!</i>
           </button>
+          
           <button type="button"onclick="toggleLock${mainDish[i].category}()" class="btn btn-success">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dice-5-fill" viewBox="0 0 16 16">
-            <path d="M8 5a1 1 0 0 1 1 1v1H7V6a1 1 0 0 1 1-1m2 2.076V6a2 2 0 1 0-4 0v1.076c-.54.166-1 .597-1 1.224v2.4c0 .816.781 1.3 1.5 1.3h3c.719 0 1.5-.484 1.5-1.3V8.3c0-.627-.46-1.058-1-1.224M6.105 8.125A.64.64 0 0 1 6.5 8h3a.64.64 0 0 1 .395.125c.085.068.105.133.105.175v2.4c0 .042-.02.107-.105.175A.64.64 0 0 1 9.5 11h-3a.64.64 0 0 1-.395-.125C6.02 10.807 6 10.742 6 10.7V8.3c0-.042.02-.107.105-.175"/>
-            <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1"/>
-          </svg>
-</svg>
+            <i class="bi bi-file-lock2-fill"></i>
             Lock It!
+            </i>
           </button>
+
           <button type="button" onclick="toggleLove${mainDish[i].category}()" class="btn btn-outline-danger">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dice-5-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M2.854 15.854A.5.5 0 0 1 2 15.5V14H.5a.5.5 0 0 1-.354-.854l1.5-1.5A.5.5 0 0 1 2 11.5h1.793l.53-.53c-.771-.802-1.328-1.58-1.704-2.32-.798-1.575-.775-2.996-.213-4.092C3.426 2.565 6.18 1.809 8 3.233c1.25-.98 2.944-.928 4.212-.152L13.292 2 12.147.854A.5.5 0 0 1 12.5 0h3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.854.354L14 2.707l-1.006 1.006c.236.248.44.531.6.845.562 1.096.585 2.517-.213 4.092-.793 1.563-2.395 3.288-5.105 5.08L8 13.912l-.276-.182a22 22 0 0 1-2.685-2.062l-.539.54V14a.5.5 0 0 1-.146.354zm2.893-4.894A20.4 20.4 0 0 0 8 12.71c2.456-1.666 3.827-3.207 4.489-4.512.679-1.34.607-2.42.215-3.185-.817-1.595-3.087-2.054-4.346-.761L8 4.62l-.358-.368c-1.259-1.293-3.53-.834-4.346.761-.392.766-.464 1.845.215 3.185.323.636.815 1.33 1.519 2.065l1.866-1.867a.5.5 0 1 1 .708.708z"/>
-</svg>
-            Love it!
+          <i class="bi bi-arrow-through-heart-fill">Love it!</i>            
           </button>
+
           <button type="button" onclick="print${mainDish[i].category}()" class="btn btn-outline-success">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dice-5-fill" viewBox="0 0 16 16">
-            <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1"/>
-            <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1"/>
-</svg>
-            Print it!
+          <i class="bi bi-printer-fill">Print it!</i>
           </button>
+
         </div>
         <img class="card-img-top" src=${mainDish[i].imageSrc} alt="Card image cap">
         <div class="card-body">
